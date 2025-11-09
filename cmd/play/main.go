98d110573f419b0339ed1c9f5019134d792048c7
jlondonobo <j.londonobo@gmail.com>
2025-11-09 17:33:03 -0500

    feat: play arbitrary sentence

---
+++ cmd/play/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "joselondono/morse/internal/sound"
+
+func main() {
+	sound.PlayWord("... --- ... / --.")
+}
+
+// Next steps: Use this in the CLI to produce single words
+// Then allow creating sentences.
---
+++ internal/sound/sound.go
@@ -0,0 +1,54 @@
+package sound
+
+import (
+	"time"
+
+	"github.com/gopxl/beep/v2"
+	"github.com/gopxl/beep/v2/generators"
+	"github.com/gopxl/beep/v2/speaker"
+)
+
+func PlayWord(s string) {
+	sr := beep.SampleRate(48000)
+	speaker.Init(sr/3, 4800)
+
+	sine, err := generators.SineTone(sr, 700) // hardcode freq for now
+	if err != nil {
+
+		panic(err)
+	}
+	silence := generators.Silence(-1)
+
+	dit := sr.N(60000000)
+	dah := dit * 3
+
+	// todo: Usin word PARIS as standard, convert control to Words Per Second
+	// todo: this is not exact correspondence. Just for convenience assuming
+	// 1-dit silence after every symbol.
+	var m = map[string]func() beep.Streamer{
+		".": func() beep.Streamer { return beep.Take(dit, sine) },
+		"-": func() beep.Streamer { return beep.Take(dah, sine) },
+		" ": func() beep.Streamer { return beep.Take(dit*2, silence) },
+		"/": func() beep.Streamer { return beep.Take(0, silence) },
+	}
+
+	ch := make(chan struct{})
+	var sounds []beep.Streamer
+
+	for _, v := range s {
+		st, ok := m[string(v)]
+		if !ok {
+			panic("Unrecognize symbol.")
+		}
+		sounds = append(sounds, st(), beep.Take(dit, silence))
+	}
+	sounds = append(sounds, beep.Callback(func() {
+		ch <- struct{}{}
+	}))
+	speaker.Play(beep.Seq(sounds...))
+	<-ch
+	time.Sleep(200 * time.Millisecond) // to ensure last signal plays
+}
+
+// todo: accept space (' ')
+// todo: accept slash ('/ ')
